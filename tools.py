203124a7255248cfc25e81420514317dcafc12f2
acazali <acazali95@gmail.com>
2026-01-05 20:45:14 -0600

    feat: add tools

---
+++ tools.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def get_actual_date_tool():
+    """
+    Returns the actual date.
+    
+    :return: actual date
+    :rtype: datetime.datetime
+    """
+    return datetime.now()
