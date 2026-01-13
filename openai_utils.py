f01aae24c1fc0b658ddad8e97970062bb105d72e
acazali <acazali95@gmail.com>
2026-01-12 23:56:50 -0600

    feat: add system_prompt to the agent

--- openai_utils.py
+++ openai_utils.py
@@ -1,12 +1,20 @@
 from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
+from finance_tools import finance_tools
+from dotenv import load_dotenv
 
-
+load_dotenv()
 
 model = ChatOpenAI(
     model="gpt-4o-mini",
     temperature=0.7
 )
 
-finantial_agent = create_agent(model, tools = [])
+finance_manager = create_agent(
+    model, 
+    tools = finance_tools,
+    system_prompt="You are a helpful finance manager/assistant to insert or retrieve spends in a database."
+)
+
+
 
