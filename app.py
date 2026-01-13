f01c516b965b608de8531dca029369a6164f506a
acazali <acazali95@gmail.com>
2026-01-12 23:56:24 -0600

    feat: improve main loop

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import os
-from openai_utils import financial_agent
+from openai_utils import finance_manager
 from dotenv import load_dotenv
+from langchain.messages import HumanMessage
 
 load_dotenv()
 if not os.getenv("OPENAI_API_KEY"):
     raise RuntimeError("OPENAI_API_KEY not found in environment variables")
 
 def main():
-
     # create model
     while(True):
         try:
             user_input = input()
+            messages = [HumanMessage(content=user_input)]
+            state = finance_manager.invoke({"messages": messages})
+            print(state['messages'])
         except Exception as ex:
             print(ex)
 
